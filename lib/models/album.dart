67387a982de0f793f3452056fce402f3bc177e63
TSM Techø <trottimusbg@gmail.com>
2019-06-13 22:07:09 +0300

    [album] Add model for album.

---
+++ lib/models/album.dart
@@ -0,0 +1,18 @@
+import 'package:our_lives/models/image.dart';
+
+class Album {
+    final String title;
+    final String imagePath;
+    final List<Image> images;
+
+    Album({this.title, this.imagePath, this.images});
+}
+
+final memeAlbum = Album(
+    title: 'Meme',
+    imagePath: 'assets/images/meme.jpg',
+    images: memeImages);
+
+final albums = [
+    memeAlbum
+];
