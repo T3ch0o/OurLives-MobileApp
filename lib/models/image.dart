c2c71969875d314e39e59baaaa5d427c89e71a5d
TSM Techø <trottimusbg@gmail.com>
2019-06-13 22:07:18 +0300

    [image] Add model for image.

---
+++ lib/models/image.dart
@@ -0,0 +1,17 @@
+class Image {
+  final String imagePath;
+  final num likes;
+
+  Image({this.imagePath, this.likes});
+}
+
+final memeImages = [
+  Image(
+    imagePath: 'assets/images/meme.jpg',
+    likes: 5
+  ),
+  Image(
+    imagePath: 'assets/images/meme.jpg',
+    likes: 8
+  ),
+];
