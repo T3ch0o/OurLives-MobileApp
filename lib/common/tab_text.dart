d671624e0544c856e2760aeabad25ac56e8dcaf9
TSM Techø <trottimusbg@gmail.com>
2019-06-14 11:42:10 +0300

    [tab_text] Add widget for the text in tab.

---
+++ lib/common/tab_text.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:our_lives/styleguides/text_styles.dart';
+
+class TabText extends StatelessWidget {
+	final String text;
+	final bool isSelected;
+	Function onTabTap;
+
+	TabText({this.text, this.isSelected, this.onTabTap});
+
+  	@override
+  	Widget build(BuildContext context) {
+    	return Transform.rotate(
+			angle: -1.58,
+			child: InkWell(
+				onTap: onTabTap,
+				child: Text(
+    	  			text,
+            		style: isSelected ? selectedTabStyle : defaultTabStyle
+    	  		),
+			),
+    	);
+  	}
+}
