de16c2a2d8ba2f85811e1e100b1b1be249932c8f
TSM Techø <trottimusbg@gmail.com>
2019-06-14 11:41:43 +0300

    [horizontal_tab_layout] Add padding and change height.

--- lib/common/horizontal_tab_layout.dart
+++ lib/common/horizontal_tab_layout.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:our_lives/styleguides/text_styles.dart';
+import 'package:our_lives/common/tab_text.dart';
 
 class HorizontalTabLayout extends StatefulWidget {
   @override
@@ -7,19 +7,56 @@ class HorizontalTabLayout extends StatefulWidget {
 }
 
 class _HorizontalTabLayoutState extends State<HorizontalTabLayout> {
-  @override
-  Widget build(BuildContext context) {
+    int selectedTabIndex = 0;
+
+    @override
+    Widget build(BuildContext context) {
     return Container(
-        height: 500,
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-              Text(
-                  'Albums',
-                  style: selectedTabStyle
-              )
-        ],
-      ),
-    );
-  }
+        height: 400,
+          child: Stack(
+                children: <Widget>[
+                    Positioned(
+                        left: 0,
+                        bottom: 0,
+                        top: 0,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 32.0),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                    TabText(
+                                        text: 'Albums',
+                                        isSelected: selectedTabIndex == 0,
+                                        onTabTap: () {
+                                            onTabTap(0);
+                                        },
+                                    ),
+                                    TabText(
+                                        text: 'Albums',
+                                        isSelected: selectedTabIndex == 1,
+                                        onTabTap: () {
+                                            onTabTap(1);
+                                        }
+                                    ),
+                                    TabText(
+                                        text: 'Albums',
+                                        isSelected: selectedTabIndex == 2,
+                                        onTabTap: () {
+                                            onTabTap(2);
+                                        }
+                                    )
+                                ],
+                            ),
+                        ),
+                    )
+                ]
+            ),
+        );
+    }
+
+    onTabTap(int index) {
+        setState(() {
+            selectedTabIndex = index;
+        });
+    }
 }
