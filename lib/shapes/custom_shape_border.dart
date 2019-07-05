bbddd0b86ac3f2b58c42eb837e6fddc2a41dfc05
TSM Techø <trottimusbg@gmail.com>
2019-07-05 13:36:34 +0300

    [custom_shape_border] Add custom shape class for the album title.

---
+++ lib/shapes/custom_shape_border.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/painting.dart';
+
+class CustomShapeBorder extends ShapeBorder {
+  @override
+  // TODO: implement dimensions
+  EdgeInsetsGeometry get dimensions => null;
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection textDirection}) {
+    // TODO: implement getInnerPath
+    return null;
+  }
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection textDirection}) {
+    // TODO: implement getOuterPath
+    return null;
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection textDirection}) {
+    // TODO: implement paint
+  }
+
+  @override
+  ShapeBorder scale(double t) {
+    // TODO: implement scale
+    return null;
+  }
+}
