bae828859902977e86713bf484156f91701c2a06
TSM Techø <trottimusbg@gmail.com>
2019-07-05 13:25:39 +0300

    [album] Change image path extension.

--- lib/models/album.dart
+++ lib/models/album.dart
@@ -1,17 +1,18 @@
 import 'package:our_lives/models/image.dart';
 
 class Album {
-    final String title;
-    final String imagePath;
-    final List<Image> images;
+  final String title;
+  final String imagePath;
+  final List<Image> images;
 
-    Album({this.title, this.imagePath, this.images});
+  Album({this.title, this.imagePath, this.images});
 }
 
 final memeAlbum = Album(
-    title: 'Meme',
-    imagePath: 'assets/images/meme.jpg',
-    images: memeImages);
+  title: 'Meme',
+  imagePath: 'assets/images/meme.png',
+  images: memeImages
+);
 
 final albums = [
     memeAlbum
