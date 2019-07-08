fdbbd2595fb567cf94cbb8450e433a1f5b8f0242
TSM Techø <trottimusbg@gmail.com>
2019-07-08 16:30:56 +0300

    [album_details_widget] Add column for the total images in the album.

--- lib/common/album_details_widget.dart
+++ lib/common/album_details_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:our_lives/models/album.dart';
+import 'package:our_lives/styleguides/text_styles.dart';
 
 import 'clippers/album_details_custom_clipper.dart';
 
@@ -15,7 +16,43 @@ class AlbumDetailsWidget extends StatelessWidget {
       clipper: AlbumDetailsCustomClipper(),
       child: Container(
         height: 180.0,
-        color: Colors.white
+        padding: const EdgeInsets.only(left: 20.0, right: 16.0, top: 30.0, bottom: 12.0),
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topRight,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.grey.withOpacity(0.4),
+                        width: 2.0                                
+                      ),            
+                    ),
+                    height: 40.0,
+                    width: 40.0,
+                    child: Center(
+                      child: Text(
+                        album.total.toString(),
+                        style: totalImagesStyle
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'new',
+                    style: labelTextStyle
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40.0,
+            )
+          ]
+        )
       )
     );
   }
