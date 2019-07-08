ee1c67c3453ee04f076328815e63c2764c799e99
TSM Techø <trottimusbg@gmail.com>
2019-07-08 17:02:52 +0300

    [horizontal_tab_layout] Add animation and other tabs content.

--- lib/common/horizontal_tab_layout.dart
+++ lib/common/horizontal_tab_layout.dart
@@ -8,68 +8,92 @@ class HorizontalTabLayout extends StatefulWidget {
   _HorizontalTabLayoutState createState() => _HorizontalTabLayoutState();
 }
 
-class _HorizontalTabLayoutState extends State<HorizontalTabLayout> {
-    int selectedTabIndex = 0;
+class _HorizontalTabLayoutState extends State<HorizontalTabLayout>
+    with SingleTickerProviderStateMixin {
+  int selectedTabIndex = 0;
+  AnimationController _controller;
+  Animation<double> _animation;
 
-    @override
-    Widget build(BuildContext context) {
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1000));
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+  }
+
+  void playAnimation() {
+    _controller.reset();
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-        height: 400,
-          child: Stack(
-                children: <Widget>[
-                    Positioned(
-                        left: 0,
-                        bottom: 0,
-                        top: 0,
-                        width: 80.0,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 32.0),
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                    TabText(
-                                        text: 'Albums',
-                                        isSelected: selectedTabIndex == 0,
-                                        onTabTap: () {
-                                            onTabTap(0);
-                                        },
-                                    ),
-                                    TabText(
-                                        text: '...',
-                                        isSelected: selectedTabIndex == 1,
-                                        onTabTap: () {
-                                            onTabTap(1);
-                                        }
-                                    ),
-                                    TabText(
-                                        text: '...',
-                                        isSelected: selectedTabIndex == 2,
-                                        onTabTap: () {
-                                            onTabTap(2);
-                                        }
-                                    )
-                                ],
-                            ),
-                        ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 60.0),
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: <Widget>[
-                          AlbumCard(album: memeAlbum),
-                          AlbumCard(album: memeAlbum)
-                        ],
-                      ),
-                    )
-                ]
+      height: 400,
+      child: Stack(children: <Widget>[
+        Positioned(
+          left: 0,
+          bottom: 0,
+          top: 0,
+          width: 80.0,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                TabText(
+                  text: 'Albums',
+                  isSelected: selectedTabIndex == 0,
+                  onTabTap: () {
+                    onTabTap(0);
+                  },
+                ),
+                TabText(
+                    text: 'Favorite',
+                    isSelected: selectedTabIndex == 1,
+                    onTabTap: () {
+                      onTabTap(1);
+                    }),
+                TabText(
+                    text: '...',
+                    // isSelected: selectedTabIndex == 2,
+                    // onTabTap: () {
+                    //   onTabTap(2);
+                    // })
+              ],
             ),
-        );
-    }
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 60.0),
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: getList(selectedTabIndex),
+          ),
+        )
+      ]),
+    );
+  }
 
-    onTabTap(int index) {
-        setState(() {
-            selectedTabIndex = index;
-        });
-    }
+  List<Widget> getList(index) {
+    return [
+      [
+        AlbumCard(album: memeAlbum),
+        AlbumCard(album: memeAlbum),
+        AlbumCard(album: memeAlbum),
+        AlbumCard(album: memeAlbum)
+      ],
+      [
+        AlbumCard(album: memeAlbum),
+        AlbumCard(album: memeAlbum)
+      ],
+    ][index];
+  }
+
+  void onTabTap(int index) {
+    setState(() {
+      selectedTabIndex = index;
+    });
+  }
 }
