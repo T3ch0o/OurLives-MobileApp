19e6053f3bb74d41797c34cd3e795dbbdb733a14
TSM Techø <trottimusbg@gmail.com>
2019-07-08 15:19:19 +0300

    [custom_shape_border] Add drawing for the text border and background.

--- lib/shapes/custom_shape_border.dart
+++ lib/shapes/custom_shape_border.dart
@@ -1,30 +1,57 @@
 import 'package:flutter/painting.dart';
 
 class CustomShapeBorder extends ShapeBorder {
+  final double distanceFromWall = 12;
+  final double controlPointDistanceFromWall = 2;
+
   @override
-  // TODO: implement dimensions
   EdgeInsetsGeometry get dimensions => null;
 
   @override
   Path getInnerPath(Rect rect, {TextDirection textDirection}) {
-    // TODO: implement getInnerPath
     return null;
   }
 
   @override
   Path getOuterPath(Rect rect, {TextDirection textDirection}) {
-    // TODO: implement getOuterPath
-    return null;
+    return getClip(Size(130.0, 60.0));
   }
 
   @override
   void paint(Canvas canvas, Rect rect, {TextDirection textDirection}) {
-    // TODO: implement paint
   }
 
   @override
   ShapeBorder scale(double t) {
-    // TODO: implement scale
     return null;
+  }
+
+  Path getClip(Size size) {
+    Path clippedPath = Path();
+
+    clippedPath.moveTo(distanceFromWall, 0);
+    clippedPath.quadraticBezierTo(controlPointDistanceFromWall,
+     controlPointDistanceFromWall, 0, distanceFromWall);
+    clippedPath.lineTo(0, size.height - distanceFromWall); 
+    clippedPath.quadraticBezierTo(controlPointDistanceFromWall,
+     size.height - controlPointDistanceFromWall, distanceFromWall, size.height);
+    clippedPath.lineTo(size.width - distanceFromWall, size.height);
+    clippedPath.quadraticBezierTo(
+      size.width - controlPointDistanceFromWall,
+      size.height - controlPointDistanceFromWall,
+      size.width,
+      size.height - distanceFromWall
+    );
+    clippedPath.lineTo(size.width, size.height * 0.6);
+    clippedPath.quadraticBezierTo(
+      size.width - 1,
+      size.height * 0.6 - distanceFromWall,
+      size.width - distanceFromWall,
+      size.height * 0.6 - distanceFromWall - 3
+    );
+    clippedPath.lineTo(distanceFromWall + 6, 0);
+
+    clippedPath.close();
+    return clippedPath;
   }
 }
