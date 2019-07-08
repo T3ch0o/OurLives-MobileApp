82d2d5a9d61226fe2ec0ba275736f76740c93e93
TSM Techø <trottimusbg@gmail.com>
2019-07-08 15:42:31 +0300

    [album_details_custom_clipper] Add drawing for the details.

---
+++ lib/common/clippers/album_details_custom_clipper.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+
+class AlbumDetailsCustomClipper extends CustomClipper<Path> {
+  final double distanceFromWall = 12;
+  final double controlPointDistanceFromWall = 2;
+
+  @override
+  getClip(Size size) {
+    final double height = size.height;
+    final double halfHeight = size.height * 0.5;
+    final double width = size.width;
+
+    Path clippedPath = Path();
+    clippedPath.moveTo(0, halfHeight);
+    clippedPath.lineTo(0, height - distanceFromWall);
+    clippedPath.quadraticBezierTo(controlPointDistanceFromWall,
+        height - controlPointDistanceFromWall, distanceFromWall, height);
+    clippedPath.lineTo(width, height);
+    clippedPath.lineTo(width, 30.0);
+    clippedPath.quadraticBezierTo(width - 5, 5.0, width - 35, 15.0);
+    clippedPath.close();
+
+    return clippedPath;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    return true;
+  }
+ 
+}
