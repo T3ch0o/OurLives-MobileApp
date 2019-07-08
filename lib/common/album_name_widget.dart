d2f260926fe3c7ba54007c49822195aba38ff827
TSM Techø <trottimusbg@gmail.com>
2019-07-08 15:18:52 +0300

    [album_name_widget] Add custom shape.

--- lib/common/album_name_widget.dart
+++ lib/common/album_name_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:our_lives/models/album.dart';
+import 'package:our_lives/shapes/custom_shape_border.dart';
+import 'package:our_lives/styleguides/colors.dart';
 import 'package:our_lives/styleguides/text_styles.dart';
 
 class AlbumNameWidget extends StatelessWidget {
@@ -11,11 +13,16 @@ class AlbumNameWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: primaryColor,
       elevation: 20.0,
-      child: Text(
-        album.title,
-        style: albumNameTextStyle,
+      shape: CustomShapeBorder(),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 18.0, left: 20.0, right: 16.0, bottom: 5.0),
+        child: Text(
+          album.title,
+          style: albumNameTextStyle,
     ),
+      ),
     );
   }
 }
