698222050aa92d7b224306c34f36fcd106b9c877
TSM Techø <trottimusbg@gmail.com>
2019-07-09 16:59:42 +0300

    [colors] Change primary color.

--- lib/styleguides/colors.dart
+++ lib/styleguides/colors.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-final Color primaryColor = Color(0xFFFF9F59);
+final Color primaryColor = Color(0xFF4A148C);
 final Color backgroundColor = Color(0xFFE4E6F1);
 final Color firstCirleColor = Colors.white.withOpacity(0.3);
 final Color secondCirleColor = Colors.white.withOpacity(0.4);
