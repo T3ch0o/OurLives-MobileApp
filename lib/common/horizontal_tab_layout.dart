8b424664fac97fb340abc68cb6e0638f61272952
TSM Techø <trottimusbg@gmail.com>
2019-07-09 17:53:19 +0300

    [horizontal_tab_layout] Remove padding.

--- lib/common/horizontal_tab_layout.dart
+++ lib/common/horizontal_tab_layout.dart
@@ -30,62 +30,59 @@ class _HorizontalTabLayoutState extends State<HorizontalTabLayout>
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 30.0),
-      child: Container(
-        height: 400.0,
-        child: Stack(children: <Widget>[
-          Positioned(
-            left: 0,
-            bottom: 0,
-            top: 0,
-            width: 80.0,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 32.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  TabText(
-                    text: 'Albums',
-                    isSelected: selectedTabIndex == 0,
+    return Container(
+      height: 400.0,
+      child: Stack(children: <Widget>[
+        Positioned(
+          left: -10,
+          bottom: 0,
+          top: 0,
+          width: 80.0,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                TabText(
+                  text: 'Albums',
+                  isSelected: selectedTabIndex == 0,
+                  onTabTap: () {
+                    onTabTap(0);
+                  },
+                ),
+                TabText(
+                    text: 'Favorite',
+                    isSelected: selectedTabIndex == 1,
                     onTabTap: () {
-                      onTabTap(0);
-                    },
-                  ),
-                  TabText(
-                      text: 'Favorite',
-                      isSelected: selectedTabIndex == 1,
-                      onTabTap: () {
-                        onTabTap(1);
-                      }),
-                  TabText(
-                      text: '...',
-                      // isSelected: selectedTabIndex == 2,
-                      // onTabTap: () {
-                      //   onTabTap(2);
-                      // }
-                    )
-                ],
-              ),
+                      onTabTap(1);
+                    }),
+                TabText(
+                    text: '...',
+                    // isSelected: selectedTabIndex == 2,
+                    // onTabTap: () {
+                    //   onTabTap(2);
+                    // }
+                  )
+              ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 80.0),
-            child: FutureBuilder(
-              future: playAnimation(),
-              builder: (context, snapshot) {
-                return SlideTransition(
-                    position: _animation,
-                    child: ListView(
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 80.0),
+          child: FutureBuilder(
+            future: playAnimation(),
+            builder: (context, snapshot) {
+              return SlideTransition(
+                  position: _animation,
+                  child: ListView(
                     scrollDirection: Axis.horizontal,
                     children: getList(selectedTabIndex),
-                  ),
-                );
-              }
-            ),
-          )
-        ]),
-      ),
+                ),
+              );
+            }
+          ),
+        )
+      ]),
     );
   }
 
