891299c4ba77a31db904d100a810061df8a21843
TSM Techø <trottimusbg@gmail.com>
2019-07-09 18:52:31 +0300

    [colors] Add colors.

--- lib/styleguides/colors.dart
+++ lib/styleguides/colors.dart
@@ -7,3 +7,7 @@ final Color backgroundColor = Color(0xFFE4E6F1);
 final Color firstCirleColor = Colors.white.withOpacity(0.3);
 final Color secondCirleColor = Colors.white.withOpacity(0.4);
 final Color thirdCirleColor = Colors.white.withOpacity(0.6);
+
+final Color firstCirleImagesPageColor = primaryColor.withOpacity(0.8);
+final Color secondCirleImagesPageColor = Colors.white.withOpacity(0.01);
+final Color thirdCirleImagesPageColor = Colors.white.withOpacity(0.02);
