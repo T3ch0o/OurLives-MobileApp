fa2583256858cf6de3607d643b59f0ce84eb1fcb
TSM Techø <trottimusbg@gmail.com>
2019-07-09 18:52:00 +0300

    [images_page] Add background and column.

--- lib/pages/images_page.dart
+++ lib/pages/images_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:our_lives/common/album_details_items_widget.dart';
+import 'package:our_lives/common/app_background.dart';
+import 'package:our_lives/common/images_background.dart';
 import 'package:our_lives/models/album.dart';
+import 'package:our_lives/styleguides/colors.dart';
+import 'package:our_lives/styleguides/text_styles.dart';
 
 class ImagesPage extends StatelessWidget {
   final Album album;
@@ -8,8 +13,50 @@ class ImagesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          AppBackground(
+            firstColor: firstCirleImagesPageColor,
+            secondColor: secondCirleImagesPageColor,
+            thirdColor: thirdCirleImagesPageColor
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 60.0,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 150.0),
+                child: AlbumDetailsItemsWidget(
+                  valueStyle: whiteValueTextStyle,
+                  labelStyle: whiteLabelTextStyle,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              ImagesBackground()
+            ],
+          )
+        ],
+      )
     );
   }
 }
