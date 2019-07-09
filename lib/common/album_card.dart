77017c2bc8f6cc700ab9c25254bc595606cabc99
TSM Techø <trottimusbg@gmail.com>
2019-07-09 17:52:55 +0300

    [album_card] Add inkwell widget and navigation to images page.

--- lib/common/album_card.dart
+++ lib/common/album_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:our_lives/models/album.dart';
+import 'package:our_lives/pages/images_page.dart';
 
 import 'album_details_widget.dart';
 import 'album_name_widget.dart';
@@ -12,37 +13,42 @@ class AlbumCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 280.0,
-      child: Card(
-        margin: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 20.0),
-        elevation: 9.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20))
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          child: Stack(
-            children: <Widget>[
-              Image.asset(
-                album.imagePath,
-                fit: BoxFit.fitWidth
-              ),
-              Positioned(
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: AlbumDetailsWidget(album: album)
-              ),
-              Positioned(
-                left: 0,
-                bottom: 90.0,
-                child: AlbumNameWidget(album: album)
-              )
-            ]
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => ImagesPage(album: album)));
+      },
+      child: SizedBox(
+        width: 280.0,
+        child: Card(
+          margin: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 20.0),
+          elevation: 9.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20))
           ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            child: Stack(
+              children: <Widget>[
+                Image.asset(
+                  album.imagePath,
+                  fit: BoxFit.fitWidth
+                ),
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: AlbumDetailsWidget(album: album)
+                ),
+                Positioned(
+                  left: 0,
+                  bottom: 90.0,
+                  child: AlbumNameWidget(album: album)
+                )
+              ]
+            ),
+          )
         )
-      )
+      ),
     );
   }
 }
