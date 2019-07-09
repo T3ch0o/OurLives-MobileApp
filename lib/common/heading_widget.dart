ce74654d476be19d3a269be578df27f08c89b399
TSM Techø <trottimusbg@gmail.com>
2019-07-09 16:57:04 +0300

    [heading_widget] Add heading.

---
+++ lib/common/heading_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:our_lives/styleguides/text_styles.dart';
+
+class HeadingWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Albums', style: headingStyle),
+          Text('Everything you want in one place!', style: subHeadingStyle)
+        ],
+      ),
+    );
+  }
+}
