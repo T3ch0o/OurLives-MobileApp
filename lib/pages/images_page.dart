abe56ba30e612b05d310a06e54f5a98cc777eadb
TSM Techø <trottimusbg@gmail.com>
2019-07-09 17:53:29 +0300

    [images_page] Add page.

---
+++ lib/pages/images_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:our_lives/models/album.dart';
+
+class ImagesPage extends StatelessWidget {
+  final Album album;
+
+  ImagesPage({this.album});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
