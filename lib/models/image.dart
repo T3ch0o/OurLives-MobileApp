1beb30beeede25bfceb72314e5a29cb6fde10215
TSM Techø <trottimusbg@gmail.com>
2019-07-10 20:34:30 +0300

    [image] Add map data functions.

--- lib/models/image.dart
+++ lib/models/image.dart
@@ -1,17 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Image {
   final String imagePath;
-  final num likes;
+  final int likes;
 
   Image({this.imagePath, this.likes});
+
+  Image.fromSnapshot(DocumentSnapshot snapshot) : this.fromMap(snapshot.data);
+
+  Image.fromMap(Map<dynamic, dynamic> map) 
+  : assert(map['imagePath'] != null),
+    assert(map['likes'] != null),
+    imagePath = map['imagePath'],
+    likes = map['likes'];
+
 }
 
 final memeImages = [
   Image(
-    imagePath: 'assets/images/meme.jpg',
+    imagePath: 'assets/images/meme.png',
     likes: 5
   ),
   Image(
-    imagePath: 'assets/images/meme.jpg',
+    imagePath: 'assets/images/profile.jpg',
     likes: 8
-  ),
+  )
 ];
