d77bacc05c8475fdf4e2fb44b20afb5315790185
TSM Techø <trottimusbg@gmail.com>
2019-07-10 20:35:06 +0300

    [main] Initialize firebase service.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:our_lives/pages/landing_page.dart';
 
+import 'api/firebase_service.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FirebaseService firebaseService = new FirebaseService();
       return MaterialApp(
           title: 'Our Lives',
           theme: ThemeData(
               primarySwatch: Colors.blue
           ),
           debugShowCheckedModeBanner: false,
-          home: LandingPage()
+          home: LandingPage(firebaseService: firebaseService)
       );
   }
 }
