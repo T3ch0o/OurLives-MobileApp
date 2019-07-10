62445bd06ad25dc408934e3898bdcb2f071178f3
TSM Techø <trottimusbg@gmail.com>
2019-07-10 20:34:19 +0300

    [album] Add map data functions.

--- lib/models/album.dart
+++ lib/models/album.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:our_lives/models/image.dart';
 
 class Album {
@@ -7,13 +8,24 @@ class Album {
   final int totalImages;
 
   Album({this.title, this.imagePath, this.images, this.totalImages});
+
+  Album.fromSnapshot(DocumentSnapshot snapshot) : this.fromMap(snapshot.data);
+
+  Album.fromMap(Map<dynamic, dynamic> map) 
+  : assert(map['title'] != null),
+    assert(map['imagePath'] != null),
+    assert(map['images'] != null),
+    title = map['title'],
+    imagePath = map['imagePath'],
+    images = (map['images'] as List).map((image) => Image.fromMap(image)).toList(),
+    totalImages = map['totalImages'];
 }
 
 final memeAlbum = Album(
   title: 'Meme',
   imagePath: 'assets/images/meme.png',
   images: memeImages,
-  totalImages: 20
+  totalImages: 2,
 );
 
 final albums = [
