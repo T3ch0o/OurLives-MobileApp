1c2a20e7a142dc3e8a2b5f1987b7afb98b58edca
TSM Techø <trottimusbg@gmail.com>
2019-07-10 15:33:32 +0300

    [images_background] Change widget to positioned.

--- lib/common/images_background.dart
+++ lib/common/images_background.dart
@@ -2,24 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:our_lives/styleguides/colors.dart';
 
 class ImagesBackground extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-        builder: (context, constraint) {
-            final height = constraint.maxHeight;
-            final width = constraint.maxWidth;
+      builder: (context, constraint) {
+        double width = constraint.maxWidth;
 
-            var container = Container(
-              decoration: BoxDecoration(
-                color: backgroundColor,
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(100.0))
-              ),
-              width: width,
-              height: 500.0
-            );
-            return container;
-        },
+        Widget container = Stack(
+          children: <Widget>[
+            Positioned(
+              top: 190.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: backgroundColor,
+                  borderRadius:
+                    BorderRadius.only(topLeft: Radius.circular(100.0))),
+                width: width,
+                height: 500.0
+              )
+            )
+          ]
+        );
+
+        return container;
+      },
     );
   }
 }
