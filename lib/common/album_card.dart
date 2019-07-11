a227699540e4de8b44d9e8999bdb11ab0a5eebb1
TSM Techø <trottimusbg@gmail.com>
2019-07-11 22:42:26 +0300

    [album_card] Add CachedNetworkImageProvider.

--- lib/common/album_card.dart
+++ lib/common/album_card.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:our_lives/api/firebase_service.dart';
 import 'package:our_lives/models/album.dart';
 import 'package:our_lives/pages/images_page.dart';
 
@@ -7,11 +9,14 @@ import 'album_name_widget.dart';
 
 class AlbumCard extends StatelessWidget {
   final Album album;
+  final FirebaseService firebaseSerivce;
+  final String albumId;
 
-  AlbumCard({this.album});
+  AlbumCard({this.album, this.firebaseSerivce, this.albumId});
 
   @override
   Widget build(BuildContext context) {
+
     return Material(
       color: Colors.transparent,
       child: InkWell(
@@ -19,20 +24,20 @@ class AlbumCard extends StatelessWidget {
           Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (context) => ImagesPage(album: album)));
+                  builder: (context) => ImagesPage(album: album, firebaseService: firebaseSerivce, albumId: albumId)));
         },
         child: SizedBox(
-            width: 280.0,
+            width: 80.0,
             child: Card(
                 margin: const EdgeInsets.symmetric(
-                    horizontal: 25.0, vertical: 20.0),
+                    horizontal: 50.0, vertical: 20.0),
                 elevation: 9.0,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(20))),
                 child: ClipRRect(
                   borderRadius: BorderRadius.all(Radius.circular(20)),
                   child: Stack(children: <Widget>[
-                    Image.asset(album.imagePath, fit: BoxFit.fitWidth),
+                    Image(image: CachedNetworkImageProvider(album.imagePath), fit: BoxFit.fitWidth),
                     Positioned(
                         left: 0,
                         right: 0,
