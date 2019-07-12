257af1f32f49124802fbacb181bbdf20a46d4661
TSM Techø <trottimusbg@gmail.com>
2019-07-12 11:21:58 +0300

    [main] Change theme color.

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:our_lives/pages/landing_page.dart';
 
 import 'api/firebase_service.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      
+    ));
+
     FirebaseService firebaseService = new FirebaseService();
       return MaterialApp(
           title: 'Our Lives',
           theme: ThemeData(
-              primarySwatch: Colors.blue
+              primarySwatch: Colors.deepPurple
           ),
           debugShowCheckedModeBanner: false,
           home: LandingPage(firebaseService: firebaseService)
